2b5db6608d54fb23fc223a88522ff7b1192615cd
mohrip <m25srt@gmail.com>
2024-10-21 15:32:08 +0300

    ""

--- src/USERS/users/createUser.dto.ts
+++ src/USERS/users/createUser.dto.ts
@@ -1,4 +1,4 @@
-import {IsEmail , IsNotEmpty , IsString , MinLength} from 'class-validator';
+import {IsEmail , IsNotEmpty, MinLength} from 'class-validator';
 
 export class CreateUserDto {
     @IsEmail()
@@ -10,4 +10,6 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   role: string; 
+
+
 }
