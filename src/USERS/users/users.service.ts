551a1dbe7da9662a5ca6213aac97bd8890ec8a8e
mohrip <m25srt@gmail.com>
2024-10-21 15:40:37 +0300

    ""

--- src/USERS/users/users.service.ts
+++ src/USERS/users/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/users.entity';
+import { CreateUserDto } from '../users/createUser.dto';
+import { UpdateUserDto } from '../users/updateUser.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { email, password, role } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({ email, password: hashedPassword, role });
+    return this.usersRepository.save(user);
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<void> {
+    const { password, profile } = updateUserDto;
+    const hashedPassword = password ? await bcrypt.hash(password, 10) : undefined;
+    await this.usersRepository.update(id, { ...(password && { password: hashedPassword }), ...(profile && { profile }) });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+}
